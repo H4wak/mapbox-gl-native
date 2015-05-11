5cab76fec353216b2ceee54f264e4d8677cb79bc
H4wak <theo.dolez@gmail.com>
2015-05-11 14:22:52 +0200

    add platform-win.gypi

--- babash.bat
+++ babash.bat
@@ -1,15 +1,8 @@
 set CONFIG_win32=-Dhost=win -Iconfig/win.gypi -Dinstall_prefix=/test
 
 set LIBS_win32=-Dheadless_lib=none -Dplatform_lib=win -Dasset_lib=zip -Dhttp_lib=curl -Dcache_lib=sqlite --depth=. -Goutput_dir=.
-set TOTO=-Dtoto="%~dp0"
+set TOTO=-Dtoto="C:\Users\test\Desktop\Fork MapBox\mapbox-gl-native"
 ::set TOTO=-Dtoto="titi"
-
-
-::LIBS_win32+=-Dasset_lib=$(word 1,$(ASSET) fs)
-::LIBS_win32+=-Dhttp_lib=$(word 1,$(HTTP) nsurl)
-::LIBS_win32+=-Dcache_lib=$(word 1,$(CACHE) sqlite)
-::LIBS_win32+=--depth=. -Goutput_dir=.
-
 
 
 
